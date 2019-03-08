ae7438d55d03b3775547c5ba475f18d4763f546f
Kylee Tilley <kyleetilley@gmail.com>
2019-03-08 10:45:51 -0600

    Adding tests

--- src/services/user.test.js
+++ src/services/user.test.js
@@ -9,40 +9,60 @@ describe("UserService", () => {
 
   let userService;
   let insertMock;
+  let firstMock;
+  let whereMock;
 
   beforeEach(() => {
     insertMock = jest.fn(() => db);
+    firstMock = jest.fn(() => db);
+    whereMock = jest.fn(() => db);
 
     db.mockImplementation(() => {
       return {
-        insert: insertMock
+        insert: insertMock,
+        first: firstMock,
+        where: whereMock
       };
     });
 
     userService = new UserService();
   });
 
-  it("should call insert correctly", async () => {
-    await userService.register(expectedUserId, expectedPassword, [
-      "foo",
-      "bar"
-    ]);
+  describe("getById", () => {
+    it("should return first found", async () => {
+      await userService.getById(expectedUserId);
+      expect(firstMock).toBeCalled();
+    });
 
-    expect(insertMock).toBeCalledWith({
-      username: expectedUserId,
-      password: expectedPassword,
-      scope: "foo bar"
+    it.skip("should call where with correct params", async () => {
+      await userService.getById(expectedUserId);
+      expect(whereMock).toBeCalledWith({ username: expectedUserId });
     });
   });
 
-  describe("when no grant is set", () => {
-    it("should not set scope", async () => {
-      await userService.register(expectedUserId, expectedPassword);
+  describe("register", () => {
+    it("should call insert correctly", async () => {
+      await userService.register(expectedUserId, expectedPassword, [
+        "foo",
+        "bar"
+      ]);
 
       expect(insertMock).toBeCalledWith({
         username: expectedUserId,
         password: expectedPassword,
-        scope: ""
+        scope: "foo bar"
+      });
+    });
+
+    describe("when no grant is set", () => {
+      it("should not set scope", async () => {
+        await userService.register(expectedUserId, expectedPassword);
+
+        expect(insertMock).toBeCalledWith({
+          username: expectedUserId,
+          password: expectedPassword,
+          scope: ""
+        });
       });
     });
   });
